4cda617d49f0c1119e62a09acf80b0055d994530
shike.miao@gmail.com <shike.miao@gmail.com>
2022-08-14 23:12:42 -0400

    env setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,19 +8,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />        
       </header>
+      <h1>Simple React Components</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Home = () => {
+    return (
+        <>
+            <main>
+                <h2>Button Component</h2>
+            </main>
+        </>
+    )
+}
